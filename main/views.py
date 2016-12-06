589e5ddf4a50e89e856f7f796c42ea7994d8922c
Alex <alexandru.oat@gmail.com>
2016-12-06 11:33:17 +0200

    Almost ready

--- main/views.py
+++ main/views.py
@@ -114,36 +114,190 @@ class Ev():
 
 class Od():
     def __init__(self, win1, win2, draw, bookmaker):
-        if "Marathonbet" in bookmaker:
-            self.win1 = self.transform_to_decimal(win1)
-            self.draw = self.transform_to_decimal(draw)
-            self.win2 = self.transform_to_decimal(win2)
-        else:
-            self.win1 = Decimal(win1).quantize(TWO_PLACES)
-            self.win2 = Decimal(win2).quantize(TWO_PLACES)
-            self.draw = Decimal(draw).quantize(TWO_PLACES)
+        # if "Marathonbet" in bookmaker:
+            # self.win1 = self.transform_to_decimal(win1)
+            # self.draw = self.transform_to_decimal(draw)
+            # self.win2 = self.transform_to_decimal(win2)
+        # else:
+        self.win1 = Decimal(win1).quantize(TWO_PLACES)
+        self.win2 = Decimal(win2).quantize(TWO_PLACES)
+        self.draw = Decimal(draw).quantize(TWO_PLACES)
         self.o1x = None
         self.o12 = None
         self.ox2 = None
         self.bookmaker = bookmaker
 
     def set_other_odds(self, o1x, o12, ox2):
-        if "Marathonbet" in self.bookmaker:
-            self.o1x = self.transform_to_decimal(o1x)
-            self.o12 = self.transform_to_decimal(o12)
-            self.ox2 = self.transform_to_decimal(ox2)
-        else:
-            self.o1x = Decimal(o1x).quantize(TWO_PLACES)
-            self.o12 = Decimal(o12).quantize(TWO_PLACES)
-            self.ox2 = Decimal(ox2).quantize(TWO_PLACES)
+        # if "Marathonbet" in self.bookmaker:
+        #     self.o1x = self.transform_to_decimal(o1x)
+        #     self.o12 = self.transform_to_decimal(o12)
+        #     self.ox2 = self.transform_to_decimal(ox2)
+        # else:
+        self.o1x = Decimal(o1x).quantize(TWO_PLACES)
+        self.o12 = Decimal(o12).quantize(TWO_PLACES)
+        self.ox2 = Decimal(ox2).quantize(TWO_PLACES)
 
     def transform_to_decimal(self, var):
         data = var.split("/")
         if len(data) == 2:
             one, two = var.split("/", 2)
             return (Decimal(one)/Decimal(two) + 1).quantize(TWO_PLACES)
-        
+
+
+
 def index(request):
+    driver = webdriver.PhantomJS()
+    driver.get("https://sports.betway.com/#/soccer/england/premier-league")             #  BetWay
+    elem_teams = driver.find_elements_by_class_name("event_name")
+    elem_odds = driver.find_elements_by_class_name("outcome_button")
+    odds = []
+    teams = []
+    events = []
+
+    for team in elem_teams:
+        data = team.text.split(" - ")
+        if len(data) == 2:
+            team1, team2 = team.text.split(" - ", 2)
+
+        teams.append(t(team1))
+        teams.append(t(team2))  
+    for el in elem_odds:
+        odds.append(el.text)
+
+    for i in range(10):
+        event = Ev(teams[i*2],teams[i*2+1])
+        od = Od(odds[i*3], odds[i*3+2], odds[i*3+1], "BetWay")
+        event.add_odds(od)
+        events.append(event)
+
+    driver.get("https://sports.bwin.com/en/sports#leagueIds=46&sportId=4")                  #  BWin
+    elem_teams = driver.find_elements_by_class_name("mb-option-button__option-name")
+    elem_odds = driver.find_elements_by_class_name("mb-option-button__option-odds") 
+    odds = []
+    teams = []
+    events2 = []
+
+    for team in elem_teams:
+        teams.append(t(team.text))
+    for el in elem_odds: 
+        odds.append(el.text)
+
+    for i in range(len(elem_teams)/3):
+        event = Ev(teams[i*3], teams[i*3+2])
+
+        od = Od(odds[i*3], odds[i*3+2], odds[i*3+1], "BWin")
+        for ev in events:
+            if ev == event:
+                ev.add_odds(od)
+        # else:
+        #   event.add_odds(od)
+        #   events.append(event)
+        event.add_odds(od)
+        events2.append(event)
+
+    
+    driver.get("https://www.marathonbet.com/en/betting/Football/England/Premier+League/?menu=21520") # Marathonbet
+    elem_teams = driver.find_elements_by_class_name('member-name')
+    elem_odds = driver.find_elements_by_class_name("price") 
+    elem_times = driver.find_elements_by_class_name("date") 
+    odds = []
+    teams = []
+    times = []
+    events3 = []
+
+    for team in elem_teams:
+        teams.append(t(team.text))
+    for el in elem_odds: 
+        odds.append(el.text)
+    for time in elem_times: 
+        times.append(time.text)
+
+    for i in range(len(elem_teams)/2):
+        event = Ev(teams[i*2], teams[i*2+1])
+        od = Od(odds[i*10], odds[i*10+2], odds[i*10+1], "Marathonbet")
+        od.set_other_odds(odds[i*10+3], odds[i*10+4], odds[i*10+5])
+        for ev in events:
+            if ev == event:
+                ev.add_odds(od)
+                ev.set_time(times[i])
+
+        event.add_odds(od)
+        events3.append(event)
+
+    #driver.get("https://www.parimatch.com/en/sport/futbol/anglija-premer-liga")
+        
+    # driver.get("https://www.fonbet.com/bets/?locale=en#11918")
+    # event3 = driver.find_elements_by_class_name('eventNumber')
+    # teams = []
+    
+    driver.get("http://olimp.com/betting/index.php?page=line&action=2&sel[]=11664") # Olimp
+    elem_teams = driver.find_elements_by_class_name("m")
+    elem_odds = driver.find_elements_by_class_name("bet_sel") 
+    
+    odds = []
+    teams = []
+    
+    events4 = []
+
+    for team in elem_teams:
+        data = team.text.split(" - ")
+        if len(data) == 2:
+            team1, team2 = team.text.split(" - ", 2)
+
+        teams.append(t(team1))
+        teams.append(t(team2))  
+    for el in elem_odds: 
+        odds.append(el.text)
+
+
+    for i in range(12):
+        event = Ev(teams[i*2], teams[i*2+1])
+        od = Od(odds[i*10], odds[i*10+2], odds[i*10+1], "Olimp")
+        od.set_other_odds(odds[i*10+3], odds[i*10+4], odds[i*10+5])
+        for ev in events:
+            if ev == event:
+                ev.add_odds(od)
+                ev.set_max()
+                ev.set_time(times[i])
+            request.session[ev.team1 + '-' + ev.team2] = ev
+
+        event.add_odds(od)
+        
+        events4.append(event)
+
+
+    context = {
+        'site_title': 'Odds aggregator',
+        'odds': odds,
+        'teams': teams,
+        'events': events,
+        'events2': events2,
+        'events3': events3,
+        'events4': events4,
+        'times': times,
+        # 'event3': len(event3),
+        # 'dates': dates,
+        # 'test': test,
+        }
+    return render(request, "main/index.html", context,)
+
+
+
+def check(request, team1, team2):
+    event = request.session[team1 + '-' + team2]#ev.team1 + '-' + ev.team2]
+    context = {
+        'site_title': 'Odds aggregator',
+        'event': event,
+        }
+    return render(request, "main/check.html", context,)
+
+
+
+
+
+
+        
+def index_old(request):
     #driver = webdriver.PhantomJS()
     # driver.get("https://sports.betway.com/#/soccer/england/premier-league")             #  BetWay
     # elem_teams = driver.find_elements_by_class_name("event_name")
@@ -292,164 +446,6 @@ def index(request):
         # 'test': test,
         }
     return render(request, "main/index.html", context,)
-
-
-
-
-
-
-
-
-
-
-
-
-
-def index_good(request):
-    driver = webdriver.PhantomJS()
-    driver.get("https://sports.betway.com/#/soccer/england/premier-league")             #  BetWay
-    elem_teams = driver.find_elements_by_class_name("event_name")
-    elem_odds = driver.find_elements_by_class_name("outcome_button")
-    odds = []
-    teams = []
-    events = []
-
-    for team in elem_teams:
-        data = team.text.split(" - ")
-        if len(data) == 2:
-            team1, team2 = team.text.split(" - ", 2)
-
-        teams.append(t(team1))
-        teams.append(t(team2))  
-    for el in elem_odds:
-        odds.append(el.text)
-
-    for i in range(10):
-        event = Ev(teams[i*2],teams[i*2+1])
-        od = Od(odds[i*3], odds[i*3+2], odds[i*3+1], "BetWay")
-        event.add_odds(od)
-        events.append(event)
-
-    driver.get("https://sports.bwin.com/en/sports#leagueIds=46&sportId=4")                  #  BWin
-    elem_teams = driver.find_elements_by_class_name("mb-option-button__option-name")
-    elem_odds = driver.find_elements_by_class_name("mb-option-button__option-odds") 
-    odds = []
-    teams = []
-    events2 = []
-
-    for team in elem_teams:
-        teams.append(t(team.text))
-    for el in elem_odds: 
-        odds.append(el.text)
-
-    for i in range(len(elem_teams)/3):
-        event = Ev(teams[i*3], teams[i*3+2])
-
-        od = Od(odds[i*3], odds[i*3+2], odds[i*3+1], "BWin")
-        for ev in events:
-            if ev == event:
-                ev.add_odds(od)
-        # else:
-        #   event.add_odds(od)
-        #   events.append(event)
-        event.add_odds(od)
-        events2.append(event)
-
-    
-    driver.get("https://www.marathonbet.com/en/betting/Football/England/Premier+League/?menu=21520") # Marathonbet
-    elem_teams = driver.find_elements_by_class_name('member-name')
-    elem_odds = driver.find_elements_by_class_name("price") 
-    elem_times = driver.find_elements_by_class_name("date") 
-    odds = []
-    teams = []
-    times = []
-    events3 = []
-
-    for team in elem_teams:
-        teams.append(t(team.text))
-    for el in elem_odds: 
-        odds.append(el.text)
-    for time in elem_times: 
-        times.append(time.text)
-
-    for i in range(len(elem_teams)/2):
-        event = Ev(teams[i*2], teams[i*2+1])
-        od = Od(odds[i*10], odds[i*10+2], odds[i*10+1], "Marathonbet")
-        od.set_other_odds(odds[i*10+3], odds[i*10+4], odds[i*10+5])
-        for ev in events:
-            if ev == event:
-                ev.add_odds(od)
-                ev.set_time(times[i])
-
-        event.add_odds(od)
-        events3.append(event)
-
-    #driver.get("https://www.parimatch.com/en/sport/futbol/anglija-premer-liga")
-        
-    # driver.get("https://www.fonbet.com/bets/?locale=en#11918")
-    # event3 = driver.find_elements_by_class_name('eventNumber')
-    # teams = []
-    
-    driver.get("http://olimp.com/betting/index.php?page=line&action=2&sel[]=11664") # Olimp
-    elem_teams = driver.find_elements_by_class_name("m")
-    elem_odds = driver.find_elements_by_class_name("bet_sel") 
-    
-    odds = []
-    teams = []
-    
-    events4 = []
-
-    for team in elem_teams:
-        data = team.text.split(" - ")
-        if len(data) == 2:
-            team1, team2 = team.text.split(" - ", 2)
-
-        teams.append(t(team1))
-        teams.append(t(team2))  
-    for el in elem_odds: 
-        odds.append(el.text)
-
-
-    for i in range(12):
-        event = Ev(teams[i*2], teams[i*2+1])
-        od = Od(odds[i*10], odds[i*10+2], odds[i*10+1], "Olimp")
-        od.set_other_odds(odds[i*10+3], odds[i*10+4], odds[i*10+5])
-        for ev in events:
-            if ev == event:
-                ev.add_odds(od)
-                ev.set_max()
-                ev.set_time(times[i])
-            request.session[ev.team1 + '-' + ev.team2] = ev
-
-        event.add_odds(od)
-        
-        events4.append(event)
-
-
-    context = {
-        'site_title': 'Odds aggregator',
-        'odds': odds,
-        'teams': teams,
-        'events': events,
-        'events2': events2,
-        'events3': events3,
-        'events4': events4,
-        'times': times,
-        # 'event3': len(event3),
-        # 'dates': dates,
-        # 'test': test,
-        }
-    return render(request, "main/index.html", context,)
-
-
-
-def check(request, team1, team2):
-    event = request.session[team1 + '-' + team2]#ev.team1 + '-' + ev.team2]
-    context = {
-        'site_title': 'Odds aggregator',
-        'event': event,
-        }
-    return render(request, "main/check.html", context,)
 
 
 
